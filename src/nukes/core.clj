1bf60d877c1abea09a0634287c895395191d547a
Arufonsekun <junior.ramisch@gmail.com>
2022-09-14 20:50:11 -0300

    feat: Adds basic operators module

--- src/nukes/core.clj
+++ src/nukes/core.clj
@@ -1,58 +1,10 @@
 (ns nukes.core
-  (:gen-class))
+  (:gen-class) 
+  (:require [nukes.basic-operators :as basic-operators])
+  (:require [nukes.data-structures :as ds]))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm little nuke"))
-
-
-(defn flow-control
-  "Control flow example, else branch is OPTIONAL"
-  [a]
-  (if a
-    "The passed parameter is truthy"
-    "The passed parameter is falsey"))
-
-
-(defn the-do
-  "Do operator wraps up multiple forms in parentheses and run each of then"
-  [num]
-  (if (= (mod num 2) 0)
-    (do
-      (println "Given number is even")
-      "Well done")
-    (do
-      (println "Given number is odd")
-      "Good luck next time ;)")
-    ))
-
-(defn the-when
-  "'when' it's like 'if' but with no else branch. Use it when you what to do many things when a cond is true and you whant to return nil if cond is false"
-  [something]
-  (when something
-    (println "Something was truthy evaluated")
-    "Yaaas"))
-
-(defn is-nil
-  "Checks is something is nil"
-  [something]
-  (nil? something))
-
-
-(defn is-equals
-  "Checks whether two things are equal or not"
-  [thing1, thing2]
-  (= thing1 thing2))
-
-
-; Amazing online moba game nicknames
-(def awesome-moba-nicknames ["Arufonsekun" "Nukes Nofura"])
-
-(defn greet-person
-  "Greets the person given a person code"
-  [person-code]
-  (str "Be very welcome sir " 
-       (if (= person-code 1)
-         (first awesome-moba-nicknames)
-         (second awesome-moba-nicknames))))
